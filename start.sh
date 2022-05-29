88e04720c3250eed2b23b28d78fa6eb9870a7a8d
dinaau <68769882+dinaau@users.noreply.github.com>
2022-05-30 00:00:14 +1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@
 # configs
 AUUID=3ecdf8aa-0f69-4791-9def-19eaab5d05ba
 CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
-CONFIGCADDY=https://raw.githubusercontent.com/jone354df465/Railway-Xray1/master/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/jone354df465/Railway-Xray1/master/etc/xray.json
+CONFIGCADDY=https://raw.githubusercontent.com/dinaau/Railway-Xray1/master/etc/Caddyfile
+CONFIGXRAY=https://raw.githubusercontent.com/dinaau/Railway-Xray1/master/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
-StoreFiles=https://raw.githubusercontent.com/jone354df465/Railway-Xray1/master/etc/StoreFiles
+StoreFiles=https://raw.githubusercontent.com/dinaau/Railway-Xray1/master/etc/StoreFiles
 #PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
